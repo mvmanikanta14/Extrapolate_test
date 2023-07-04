fac31bc3087c54e6fe9dfb9b00d972811f25db7e
Mohd Quddus <110477881+mdquddus7@users.noreply.github.com>
2023-07-04 15:57:59 +0530

    changes_q

---
+++ src/pages/listing/Pricingconcessions.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+
+import Badge from "react-bootstrap/Badge";
+import {
+  BsChevronDown,
+  BsBriefcase,
+  BsSpeedometer2,
+  BsChevronUp,
+} from "react-icons/bs";
+import Card from "react-bootstrap/Card";
+
+
+const Pricingconcessions = () => {
+
+
+  return (
+    <section>
+    
+ 
+    <div className="container-fluid"> 
+  
+    <div className="content">
+    <div className="col-md-4 col-lg-4 col-sm-12">
+              <Card>
+                <Card.Body>
+                  <div className="cat-icon">
+                    {" "}
+                    <span className="bg-1">
+                      {" "}
+                      <BsBriefcase />
+                    </span>{" "}
+                  </div>
+                  <div className="card-details">
+                    <Card.Title className="text-truncate">
+                    Pricing and Concessions 
+                    </Card.Title>
+                    <p className="cards-count">
+                    
+                      
+                      <Badge bg="warning-transpernt" className="float-right">
+                        {" "}
+                       
+                      </Badge>{" "}
+                    </p>
+                    <span> Coming Soon </span>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>  
+      </div> 
+    </div>
+  </section>
+  );
+};
+
+export default Pricingconcessions;
---
+++ src/pages/listing/Proposalinbrief.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+import Navbar from "../Navbar";
+import Card from "react-bootstrap/Card";
+import {
+    BsChevronDown,
+    BsBriefcase,
+    BsSpeedometer2,
+    BsChevronUp,
+  } from "react-icons/bs";
+import Badge from "react-bootstrap/Badge";
+
+
+
+
+
+const Proposalinbrief = () => {
+  
+
+
+
+  return (
+    <section>
+    
+ 
+        <div className="container-fluid"> 
+      
+       <div className="content">
+       <div className="col-md-4 col-lg-4 col-sm-12">
+              <Card>
+                <Card.Body>
+                  <div className="cat-icon">
+                    {" "}
+                    <span className="bg-1">
+                      {" "}
+                      <BsBriefcase />
+                    </span>{" "}
+                  </div>
+                  <div className="card-details">
+                    <Card.Title className="text-truncate">
+                    Proposal in Brief 
+                    </Card.Title>
+                    <p className="cards-count">
+                    
+                      
+                      <Badge bg="warning-transpernt" className="float-right">
+                        {" "}
+                       
+                      </Badge>{" "}
+                    </p>
+                    <span> Coming Soon  </span>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>      </div> 
+             </div>
+      </section>
+  );
+};
+
+export default Proposalinbrief;
