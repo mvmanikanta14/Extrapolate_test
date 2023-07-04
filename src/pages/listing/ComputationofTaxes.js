948db67ff6c0bb4965f44b297ad078355203e91a
madhukant <madhukantsingh15@gmai.com>
2023-07-04 13:40:53 +0530

    changes

---
+++ src/pages/listing/ComputationofTaxes.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+import Badge from "react-bootstrap/Badge";
+import {
+  BsChevronDown,
+  BsBriefcase,
+  BsSpeedometer2,
+  BsChevronUp,
+} from "react-icons/bs";
+import Card from "react-bootstrap/Card";
+
+
+
+const ComputationofTaxes = () => {
+
+
+  return (
+    <section>
+    
+ 
+        <div className="container-fluid"> 
+      
+        <div className="content">
+        <div className="col-md-4 col-lg-4 col-sm-12">
+              <Card>
+                <Card.Body>
+                  <div className="cat-icon">
+                    {" "}
+                    <span className="bg-1">
+                      {" "}
+                      <BsBriefcase />
+                    </span>{" "}
+                  </div>
+                  <div className="card-details">
+                    <Card.Title className="text-truncate">
+                    Computation of Taxes
+                    </Card.Title>
+                    <p className="cards-count">
+                    
+                      
+                      <Badge bg="warning-transpernt" className="float-right">
+                        {" "}
+                       
+                      </Badge>{" "}
+                    </p>
+                    <span> Computation of Taxes</span>
+                  </div>
+                </Card.Body>
+              </Card>
+            </div>      
+          </div> 
+        </div>
+      </section>
+  );
+};
+
+export default ComputationofTaxes;
