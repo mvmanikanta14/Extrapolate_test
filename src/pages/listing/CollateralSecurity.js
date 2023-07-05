17d6a9f035ed17cf427e9b86a3105e983f8ddbe4
Anil <kadarianilk@gmail.com>
2023-07-05 12:25:33 +0530

    changes

--- src/pages/listing/CollateralSecurity.js
+++ src/pages/listing/CollateralSecurity.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 
 import Card from "react-bootstrap/Card";
 import {
@@ -9,49 +9,85 @@ import {
   } from "react-icons/bs";
 import Badge from "react-bootstrap/Badge";
 
+import Popup from 'reactjs-popup';
+import { useModal } from 'react-hooks-use-modal';
 
 
 
 const CollateralSecurity = () => {
+ 
+  const [Modal, open, close, isOpen] = useModal('root', {
+    preventScroll: true,
+    closeOnOverlayClick: false
+  });
   
 
-
-
-  return (
-    <section>
-    
+  
  
+  return (
+      <section>
+   
+           
         <div className="container-fluid"> 
       
-       <div className="content">
-       <div className="col-md-4 col-lg-4 col-sm-12">
-              <Card>
-                <Card.Body>
-                  <div className="cat-icon">
-                    {" "}
-                    <span className="bg-1">
+            <div className="content">
+              <div>
+                <p> {isOpen ? '' : ''}</p>
+                <button onClick={open} className="border-0">Add</button>
+                <div className="modal-dialog modal-custom-mx">
+                  <Modal>
+                    <Card style={{ width: '300px', height: '300px' }}>
+                      <Card.Body>
+
+                        <Card.Title className="row mb-0">
+                          <h4 className="col-md-6">Add</h4>
+                          {/* <button className="col-md-2 float-right" onClick={close}>x</button> */}
+                          <div className="col-md-6 ">
+                          <button className="float-right cardbutton" onClick={close}>x</button>
+                          </div>
+                        </Card.Title>
+                        <label className="">Name</label>
+                        <input type="text" value="" /><br/>
+                        <label className="">Adderess</label>
+                        <input type="text" className="mt-0"/><br/><br/>
+                        <input type="Submit" className="cardbutton"/>
+                      </Card.Body>
+                    </Card>
+                  </Modal>
+                </div>
+              </div>
+              <div className="col-md-4 col-lg-4 col-sm-12">
+                <Card>
+                  <Card.Body>
+                    <div className="cat-icon">
                       {" "}
-                      <BsBriefcase />
-                    </span>{" "}
-                  </div>
-                  <div className="card-details">
-                    <Card.Title className="text-truncate">
-                    Collateral Security
-                    </Card.Title>
-                    <p className="cards-count">
-                    
-                      
-                      <Badge bg="warning-transpernt" className="float-right">
+                      <span className="bg-1">
                         {" "}
-                       
-                      </Badge>{" "}
-                    </p>
-                    <span> Coming Soon </span>
-                  </div>
-                </Card.Body>
-              </Card>
-            </div>     </div> 
-             </div>
+                        <BsBriefcase />
+                      </span>{" "}
+                    </div>
+                    <div className="card-details">
+                      <Card.Title className="text-truncate">
+                      collateral Security
+                      </Card.Title>
+                      <p className="cards-count">
+                      
+                        
+                        <Badge bg="warning-transpernt" className="float-right">
+                          {" "}
+                        
+                        </Badge>{" "}
+                      </p>
+                      <span> Coming Soon </span>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </div>  
+                
+            </div> 
+        </div>
+        
+        
       </section>
   );
 };
